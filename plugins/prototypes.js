ff4093872ec583410c983cfaca1607d8e5fc5542
Akintomide Alonge <akintomide.alonge@gmail..com>
2021-09-12 14:46:52 +0100

    removed splitting

--- plugins/prototypes.js
+++ plugins/prototypes.js
@@ -1,15 +1,8 @@
 import Vue from 'vue'
 
-import Splitting from 'splitting'
 import VueToast from 'vue-toast-notification'
 
-import 'splitting/dist/splitting.css'
-import 'splitting/dist/splitting-cells.css'
-
 import 'vue-toast-notification/dist/theme-default.css'
-
-Splitting()
 // import 'vue-toast-notification/dist/theme-sugar.css';
 
 Vue.use(VueToast)
-Vue.prototype.$splitting = Splitting
